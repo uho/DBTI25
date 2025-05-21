2d0295d60a7a8c9f85bf57fb5bd1ca59047f435a
Ulrich Hoffmann <uho@xlerb.de>
2025-05-21 15:53:09 +0000

    Fix container stratup

--- .devcontainer/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Startet den MariaDB-Dienst und eine Bash-Shell
-
-# Starte MariaDB
-service mariadb start
-
-# Halte den Container offen
-exec bash
-
---
+++ .devcontainer/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Starte MariaDB
+echo "⚙️ Starte MariaDB..."
+service mariadb start
